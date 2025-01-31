aa79745d6398d402adc991d4f52dd79d7965769c
billsky <billsky14@gmail.com>
2025-01-31 18:44:48 +0700

    ignore eslint during build

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 
 export async function GET() {
   return NextResponse.json([{
-    message: "Otakudesu unofficial API, made by rizkyhaksono with 🤍",
+    message: "Credit to by rizkyhaksono",
     GitHub: "https://github.com/rizkyhaksono",
     Support: "https://saweria.co/natee",
   },],
